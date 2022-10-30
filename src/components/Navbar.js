c3a81fa49b834d63e56ef5155b0674b66006c2dd
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-10-30 01:52:32 -0400

    Add skeleton for Education and Experience Sections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
+// import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
+        {/* <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Yunus Mohammed
           </a>
-        </a>
+        </a> */}
         <nav className="md:ml-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#home" className="mr-5 hover:text-white">
-            Home
+          <a href="#about" className="mr-5 hover:text-white">
+            About
           </a>
           <a href="#education" className="mr-5 hover:text-white">
             Education
@@ -26,9 +26,9 @@ export default function Navbar() {
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white ">
+          {/* <a href="#testimonials" className="mr-5 hover:text-white ">
             Testimonials
-          </a>
+          </a> */}
           <a href="#contacts" className="mr-5 hover:text-white ">
             Contacts
           </a>
