c2369b21c2312e440290f079a04887a9321c1a5d
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-11-02 17:18:50 -0400

    Complete About Section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contacts";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
+import React from 'react'
+import About from './components/About'
+import Contact from './components/Contacts'
+import Education from './components/Education'
+import Experience from './components/Experience'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Testimonials from './components/Testimonials'
 
-export default function App() {
+export default function App () {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className='text-white body-font'>
       <Navbar />
       <About />
       <Education />
@@ -20,5 +20,5 @@ export default function App() {
       <Testimonials />
       <Contact />
     </main>
-  );
+  )
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,125 @@
-import React from "react";
+import React from 'react'
 
-export default function About() {
+export default function About () {
   return (
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Yunus Mohammed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
-          </p>
-          <div className="flex justify-center">
+    <section id='about' className='bg-gray-900'>
+      <div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
+        <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
+          <div className='flex justify-center py-10'>
+            {/* Github */}
             <a
-              href="#contacts"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              href='./yunusmohammedresume.pdf'
+              download
+              className='inline-flex mr-5 bg-white border-2 border-white focus:outline-none hover:bg-blue-600 rounded-full text-lg'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='48'
+                height='48'
+                viewBox='0 0 24 24'
+                fill='#111827'
+              >
+                <path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' />
+              </svg>
             </a>
+            {/* Email */}
             <a
+              href='mailto:ymm26@cornell.edu'
+              className='inline-flex mr-5 bg-white border-2 border-white focus:outline-none hover:bg-blue-600 rounded-full text-lg'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='48'
+                height='48'
+                viewBox='0 0 24 24'
+                fill='#111827'
+              >
+                <path d='M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z' />
+              </svg>
+            </a>
+            {/* LinkedIn */}
+            <a
+              href='https://www.linkedin.com/in/yunus-mohammed-8a7941199'
+              className='inline-flex bg-white border-2 border-white focus:outline-none hover:bg-blue-600 rounded-full text-lg'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='48'
+                height='48'
+                viewBox='0 0 24 24'
+                fill='#111827'
+              >
+                <path d='M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z' />
+              </svg>
+            </a>
+          </div>
+          <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
+            Hi, I'm Yunus Mohammed.
+            {/* <br className="hidden lg:inline-block" />I love to build amazing
+            apps. */}
+          </h1>
+          <p className='mb-8 leading-relaxed'>
+            A computer science student at Cornell University with experience in
+            full stack software engineering at top companies like Microsoft and
+            Intuit.
+          </p>
+          <div className='flex justify-center'>
+            <a
+              href='./yunusmohammedresume.pdf'
+              download
+              className='inline-flex text-white border-2 border-white py-2 px-6 focus:outline-none hover:bg-blue-600 rounded-full text-lg'
+            >
+              <p className='mr-2'>Resume</p>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                class='w-6 h-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3'
+                />
+              </svg>
+            </a>
+            {/* <button
+              id='downloadResumeBtn'
+              class='bg-transparent hover:bg-grey py-2 px-4 border-2 border-white text-white text-lg rounded-full inline-flex items-center'
+            >
+              <span class='mr-2'>Resume</span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                class='w-6 h-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3'
+                />
+              </svg>
+            </button> */}
+            {/* <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               See My Past Work
-            </a>
+            </a> */}
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
           <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
+            className='object-cover object-center rounded'
+            alt='hero'
+            src='./headshot8.png'
           />
         </div>
       </div>
     </section>
-  );
+  )
 }
