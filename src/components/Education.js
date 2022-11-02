a8baf945c20a43fe0f08f27de9a3b750a8a248d2
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-11-02 17:49:20 -0400

    configure github pages

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,17 +1,17 @@
-import { CodeIcon } from "@heroicons/react/solid";
-import React from "react";
+import { CodeIcon } from '@heroicons/react/solid'
+import React from 'react'
 // import { projects } from "../data";
 
-export default function Projects() {
+export default function Projects () {
   return (
-    <section id="education" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+    <section id='education' className='text-black bg-white body-font'>
+      <div className='container px-5 py-10 mx-auto text-center lg:px-40'>
+        <div className='flex flex-col w-full mb-20'>
+          <CodeIcon className='mx-auto inline-block w-10 mb-4' />
+          <h1 className='sm:text-4xl text-3xl font-medium title-font mb-4 text-black'>
             Education
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className='lg:w-2/3 mx-auto leading-relaxed text-base'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
             facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
             fuga dolore.
@@ -19,5 +19,5 @@ export default function Projects() {
         </div>
       </div>
     </section>
-  );
+  )
 }
