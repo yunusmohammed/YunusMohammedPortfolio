1d30a81152587d76198f96d298fd2cdf4ceced69
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-11-08 21:44:30 -0500

    Experience Section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -33,16 +33,81 @@ export default function Projects () {
               iconClassName='vertical-timeline-element-icon1'
               icon={<BsBriefcaseFill />}
             >
-              <h3 className='vertical-timeline-element-title text-left'>
+              <h3 className='vertical-timeline-element-title text-left text-2xl font-medium'>
+                Microsoft Corporation
+              </h3>
+              <h3 className='vertical-timeline-element-title text-left font-light'>
                 Software Engineering Intern
               </h3>
-              <h4 className='vertical-timeline-element-subtitle text-left'>
+              <h4 className='vertical-timeline-element-subtitle text-left font-light'>
                 Redmond, WA
               </h4>
               <p className='text-left'>
                 I built a telemetry service using C# .NET and various components
                 in Azure to help my team understand how customers use our
                 product
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className='vertical-timeline-element-custom--work1'
+              contentStyle={{ background: 'rgb(17 24 39)', color: '#fff' }}
+              contentArrowStyle={{
+                borderRight: '7px solid  rgb(17 24 39)'
+              }}
+              date='Jan 2022 - May 2022'
+              dateClassName='text-black'
+              iconStyle={{
+                background: 'rgb(243 244 246)',
+                color: 'rgb(17 24 39)'
+              }}
+              iconClassName='vertical-timeline-element-icon1'
+              icon={<BsBriefcaseFill />}
+            >
+              <h3 className='vertical-timeline-element-title text-left text-2xl font-medium'>
+                Intuit Inc.
+              </h3>
+              <h3 className='vertical-timeline-element-title text-left font-light'>
+                Software Engineering Co-op Intern
+              </h3>
+              <h4 className='vertical-timeline-element-subtitle text-left font-light'>
+                Remote
+              </h4>
+              <p className='text-left'>
+                I built a prerequisite checker using Java and Spring/SpringBoot
+                to help my team as well as other teams determine whether they
+                can safely migrate their services to a new gateway version
+                without experiencing downtown. I also designed a service that
+                would automate these migrations for users
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className='vertical-timeline-element-custom--work1'
+              contentStyle={{ background: 'rgb(17 24 39)', color: '#fff' }}
+              contentArrowStyle={{
+                borderRight: '7px solid  rgb(17 24 39)'
+              }}
+              date='Jun 2021 - Aug 2021'
+              dateClassName='text-black'
+              iconStyle={{
+                background: 'rgb(243 244 246)',
+                color: 'rgb(17 24 39)'
+              }}
+              iconClassName='vertical-timeline-element-icon1'
+              icon={<BsBriefcaseFill />}
+            >
+              <h3 className='vertical-timeline-element-title text-left text-2xl font-medium'>
+                Microsoft Corporation
+              </h3>
+              <h3 className='vertical-timeline-element-title text-left font-light'>
+                Software Engineering Intern
+              </h3>
+              <h4 className='vertical-timeline-element-subtitle text-left font-light'>
+                Remote
+              </h4>
+              <p className='text-left'>
+                I used TypeScript and KnockoutJS to build an Azure Extension
+                that helps Azure customers to author Azure Policy for their
+                resources without needing experience in JSON.
               </p>
             </VerticalTimelineElement>
           </VerticalTimeline>
